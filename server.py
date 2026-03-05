99a434cbe4ede56da3bb23962c6fb0f894c085c5
terrentii <terrentii@vk.com>
2026-03-05 19:38:43 +0300

    Open start

--- server.py
+++ server.py
@@ -1,1 +1,12 @@
-print('Hello World!')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "VSC Web"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
